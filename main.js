282b6eca393cac9124d5676d9bd1aa117c8d57c9
marjyang <marjorie.y@hotmail.com>
2022-10-12 14:11:50 -0400

    update3

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
-const text=  document.querySelector('.text');
-text.innerHTML = text.textContent.replace(/\S/g, "<span>$&</span>");
+const text=  document.querySelector('#t1');
+text.innerHTML = text.textContent.replace(/\S/g, "<span class ='circle1'>$&</span>");
 
-const element = document.querySelectorAll('span');
-
-// see if i can create an array of elements and then do a nested for loop to create concentric circles
+const element = document.querySelectorAll('.circle1');
 
 for (let i = 0; i < element.length; i++){
     element[i].style.transform = "rotate(" + i * 20 + "deg)";
+}
+
+const text1 =  document.querySelector('#t2');
+text1.innerHTML = text1.textContent.replace(/\S/g, "<span class ='circle2'>$&</span>");
+
+const element1 = document.querySelectorAll('.circle2');
+
+for (let i = 0; i < element1.length; i++){
+    element1[i].style.transform = "rotate(" + i * 20 + "deg)";
 }
 
 // document.addEventListener("mousemove", function(e){
