7a0aa536294d07a41c71c59c7e682f834c05c634
marjyang <marjorie.y@hotmail.com>
2022-10-18 14:38:19 -0400

    up

--- js/circle.js
+++ js/circle.js
@@ -1,10 +1,38 @@
+const textim =  document.querySelector('#t-im');
+textim.innerHTML = textim.textContent.replace(/\S/g, "<span class ='circleim'>$&</span>");
+
+const elementim = document.querySelectorAll('.circleim');
+
+for (let i = 0; i < elementim.length; i++){
+    elementim[i].style.transform = "rotate(" + i * 7 + "deg)";
+}
+
+const textgd =  document.querySelector('#t-gd');
+textgd.innerHTML = textgd.textContent.replace(/\S/g, "<span class ='circlegd'>$&</span>");
+
+const elementgd = document.querySelectorAll('.circlegd');
+
+for (let i = 0; i < elementgd.length; i++){
+    elementgd[i].style.transform = "rotate(" + i * 5 + "deg)";
+}
+
+// const textoth =  document.querySelector('#t-others');
+// textoth.innerHTML = textoth.textContent.replace(/\S/g, "<span class ='circleoth'>$&</span>");
+
+// const elementoth = document.querySelectorAll('.circleoth');
+
+// for (let i = 0; i < elementoth.length; i++){
+//     elementoth[i].style.transform = "rotate(" + i * 5 + "deg)";
+// }
+
+
 const text1=  document.querySelector('#t1');
 text1.innerHTML = text1.textContent.replace(/\S/g, "<span class ='circle1'>$&</span>");
 
 const element1 = document.querySelectorAll('.circle1');
 
 for (let i = 0; i < element1.length; i++){
-    element1[i].style.transform = "rotate(" + i * 14 + "deg)";
+    element1[i].style.transform = "rotate(" + i * 10 + "deg)";
 }
 
 const text2 =  document.querySelector('#t2');
@@ -13,7 +41,7 @@ text2.innerHTML = text2.textContent.replace(/\S/g, "<span class ='circle2'>$&</s
 const element2 = document.querySelectorAll('.circle2');
 
 for (let i = 0; i < element2.length; i++){
-    element2[i].style.transform = "rotate(" + i * 13 + "deg)";
+    element2[i].style.transform = "rotate(" + i * 9.5 + "deg)";
 }
 
 const text3 =  document.querySelector('#t3');
@@ -22,7 +50,7 @@ text3.innerHTML = text3.textContent.replace(/\S/g, "<span class ='circle3'>$&</s
 const element3 = document.querySelectorAll('.circle3');
 
 for (let i = 0; i < element3.length; i++){
-    element3[i].style.transform = "rotate(" + i * 12 + "deg)";
+    element3[i].style.transform = "rotate(" + i * 9 + "deg)";
 }
 
 const text4 =  document.querySelector('#t4');
@@ -31,7 +59,7 @@ text4.innerHTML = text4.textContent.replace(/\S/g, "<span class ='circle4'>$&</s
 const element4 = document.querySelectorAll('.circle4');
 
 for (let i = 0; i < element4.length; i++){
-    element4[i].style.transform = "rotate(" + i * 11 + "deg)";
+    element4[i].style.transform = "rotate(" + i * 8.5 + "deg)";
 }
 
 const text5 =  document.querySelector('#t5');
@@ -40,7 +68,7 @@ text5.innerHTML = text5.textContent.replace(/\S/g, "<span class ='circle5'>$&</s
 const element5 = document.querySelectorAll('.circle5');
 
 for (let i = 0; i < element5.length; i++){
-    element5[i].style.transform = "rotate(" + i * 10 + "deg)";
+    element5[i].style.transform = "rotate(" + i * 8 + "deg)";
 }
 
 const text6 =  document.querySelector('#t6');
@@ -49,7 +77,7 @@ text6.innerHTML = text6.textContent.replace(/\S/g, "<span class ='circle6'>$&</s
 const element6 = document.querySelectorAll('.circle6');
 
 for (let i = 0; i < element6.length; i++){
-    element6[i].style.transform = "rotate(" + i * 9 + "deg)";
+    element6[i].style.transform = "rotate(" + i * 7.5 + "deg)";
 }
 
 const text7 =  document.querySelector('#t7');
@@ -58,7 +86,7 @@ text7.innerHTML = text7.textContent.replace(/\S/g, "<span class ='circle7'>$&</s
 const element7 = document.querySelectorAll('.circle7');
 
 for (let i = 0; i < element7.length; i++){
-    element7[i].style.transform = "rotate(" + i * 8 + "deg)";
+    element7[i].style.transform = "rotate(" + i * 7 + "deg)";
 }
 
 const text8 =  document.querySelector('#t8');
@@ -67,7 +95,7 @@ text8.innerHTML = text8.textContent.replace(/\S/g, "<span class ='circle8'>$&</s
 const element8 = document.querySelectorAll('.circle8');
 
 for (let i = 0; i < element8.length; i++){
-    element8[i].style.transform = "rotate(" + i * 7 + "deg)";
+    element8[i].style.transform = "rotate(" + i * 6.5 + "deg)";
 }
 
 const text9 =  document.querySelector('#t9');
@@ -85,10 +113,78 @@ text10.innerHTML = text10.textContent.replace(/\S/g, "<span class ='circle10'>$&
 const element10 = document.querySelectorAll('.circle10');
 
 for (let i = 0; i < element10.length; i++){
-    element10[i].style.transform = "rotate(" + i * 5 + "deg)";
+    element10[i].style.transform = "rotate(" + i * 5.5 + "deg)";
 }
 
-// document.addEventListener("mousemove", function(e){
-//     text.style.left = e.pageX + 'px';
-//     text.style.top = e.pageY + 'px';
-// });
+const text11 =  document.querySelector('#t11');
+text11.innerHTML = text11.textContent.replace(/\S/g, "<span class ='circle11'>$&</span>");
+
+const element11 = document.querySelectorAll('.circle11');
+
+for (let i = 0; i < element11.length; i++){
+    element11[i].style.transform = "rotate(" + i * 5 + "deg)";
+}
+
+const text12 =  document.querySelector('#t12');
+text12.innerHTML = text12.textContent.replace(/\S/g, "<span class ='circle12'>$&</span>");
+
+const element12 = document.querySelectorAll('.circle12');
+
+for (let i = 0; i < element12.length; i++){
+    element12[i].style.transform = "rotate(" + i * 4.5 + "deg)";
+}
+
+const text13 =  document.querySelector('#t13');
+text13.innerHTML = text13.textContent.replace(/\S/g, "<span class ='circle13'>$&</span>");
+
+const element13 = document.querySelectorAll('.circle13');
+
+for (let i = 0; i < element13.length; i++){
+    element13[i].style.transform = "rotate(" + i * 4 + "deg)";
+}
+
+const text14 =  document.querySelector('#t14');
+text14.innerHTML = text14.textContent.replace(/\S/g, "<span class ='circle14'>$&</span>");
+
+const element14 = document.querySelectorAll('.circle14');
+
+for (let i = 0; i < element14.length; i++){
+    element14[i].style.transform = "rotate(" + i * 3.5 + "deg)";
+}
+
+
+const text15 =  document.querySelector('#t15');
+text15.innerHTML = text15.textContent.replace(/\S/g, "<span class ='circle15'>$&</span>");
+
+const element15 = document.querySelectorAll('.circle15');
+
+for (let i = 0; i < element15.length; i++){
+    element15[i].style.transform = "rotate(" + i * 3 + "deg)";
+}
+
+const text16 =  document.querySelector('#t16');
+text16.innerHTML = text16.textContent.replace(/\S/g, "<span class ='circle16'>$&</span>");
+
+const element16 = document.querySelectorAll('.circle16');
+
+for (let i = 0; i < element16.length; i++){
+    element16[i].style.transform = "rotate(" + i * 2.5 + "deg)";
+}
+
+const text17 =  document.querySelector('#t17');
+text17.innerHTML = text17.textContent.replace(/\S/g, "<span class ='circle17'>$&</span>");
+
+const element17 = document.querySelectorAll('.circle17');
+
+for (let i = 0; i < element17.length; i++){
+    element17[i].style.transform = "rotate(" + i * 2 + "deg)";
+}
+
+const text18 =  document.querySelector('#t18');
+text18.innerHTML = text17.textContent.replace(/\S/g, "<span class ='circle18'>$&</span>");
+
+const element18 = document.querySelectorAll('.circle18');
+
+for (let i = 0; i < element18.length; i++){
+    element18[i].style.transform = "rotate(" + i * 1.5 + "deg)";
+}
