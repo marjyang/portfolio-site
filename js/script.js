776d6c1c6ef51dc8528c146d3034aae668c3ce84
marjyang <marjorie.y@hotmail.com>
2022-11-07 18:26:28 -0500

    update

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// const cursor = document.querySelector('.circle');
+
+// const moveCursor = (e)=> {
+//     const mouseY = e.clientY;
+//     const mouseX = e.clientX;
+    
+//     cursor.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+   
+//   }
+  
+// window.addEventListener('mousemove', moveCursor)
+
+let imBox = document.getElementsByClassName('im-container');
+
+imBox.addEventListener('mouseover', (e) => {});
+
+const onmouseover = (e) => { 
+    
+};
