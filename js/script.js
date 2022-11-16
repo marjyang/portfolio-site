c5540c862c2770257bf00c320f81304087fec7ac
marjyang <marjorie.y@hotmail.com>
2022-11-15 21:14:47 -0500

    update

--- js/script.js
+++ js/script.js
@@ -16,6 +16,8 @@ for (let i = 0; i < links.length; i++){
     });
 }
 
+
+
 // take user back to top
 const buttonToTop = document.querySelector('#scrollToTop');
 
@@ -23,3 +25,32 @@ buttonToTop.addEventListener('click', () =>{
     window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
 });
 
+
+//heyzine api
+
+
+
+//contact form
+
+
+// const form = document.querySelector('#form-link');
+// let contactForm = document.querySelector('#contact-form');
+// let shadow = document.querySelector('.shadow');
+
+// form.addEventListener('click', () => {
+//     contactForm.classList.add('display');
+//     shadow.classList.add('display');
+// })
+
+
+// //lightbox
+// const lightbox = document.querySelector('#lightbox');
+// const images = document.querySelectorAll('.sketch-image');
+// images.forEach(image => {
+//     image.addEventListener('click', e => {
+//         lightbox.classList.add('lightbox-active');
+//         const img = document.createElement('img');
+//         img.src = image.src;
+//         lightbox.appendChild(img);
+//     });
+// });
