3dc68ef501c81702cadb5dedb8705fdd35868a13
marjyang <marjorie.y@hotmail.com>
2022-12-09 18:09:28 -0500

    cms api update

--- js/contentful.js
+++ js/contentful.js
@@ -4,41 +4,73 @@ var client = contentful.createClient({
 });
 
 client.getEntries().then((entries) =>{
+    let projCover1 = document.getElementById('project-cover1');
+    let projCover2 = document.getElementById('project-cover2');
+    let projCover3 = document.getElementById('project-cover3');
+    let projCover4 = document.getElementById('project-cover4');
+    let projCover5 = document.getElementById('project-cover5');
+    let projCover6 = document.getElementById('project-cover6');
 
     let project1 = document.getElementById('project1');
     let project2 = document.getElementById('project2');
     let project3 = document.getElementById('project3');
     let project4 = document.getElementById('project4');
+    let project5 = document.getElementById('project5');
+    let project6 = document.getElementById('project6');
 
+    let html = [];
+    let covers = [];
+    let filteredHtml = [];
+    let filteredCovers = [];
 
     entries.items.forEach((entry) => {
-        let html = `
+        html[entry] = `
             <h1>${entry.fields.title}</h1>
-            <img class='img' src='${entry.fields.image.fields.file.url}'>
-            <p>${entry.fields.body}</p>
+            <div class = "indiv-medium">${entry.fields.medium}</div>
+            <div class = "indiv-role">${entry.fields.role}</div>
         `;
         
-        project1.innerHTML = html;
-        project2.innerHTML = html;
-        project3.innerHTML = html;
-        project4.innerHTML = html;
-
+        covers[entry] = `
+            <div class="image">
+                <img class ="image-img" src=${entry.fields.thumbnailImage.fields.file.url}>
+                <div class = "image-overlay image-overlay-blur">
+                    <div class="image-title">${entry.fields.title}</div>
+                    <p class = "image-desc">${entry.fields.medium}</p>
+                    <p class = "image-desc">${entry.fields.role}</p>
+                </div>
+            </div>
+        `;
+        
+        console.log(entry.fields);
+        html.push(html[entry]);
+        covers.push(covers[entry]);
     });
+
+    projCover1.innerHTML = covers[1];
+    projCover2.innerHTML = covers[0];
+    projCover3.innerHTML = covers[5];
+    projCover4.innerHTML = covers[4];
+    projCover5.innerHTML = covers[3];
+    projCover6.innerHTML = covers[2];
+
+    project1.innerHTML = html[1];
+    project2.innerHTML = html[0];
+    project3.innerHTML = html[5];
+    project4.innerHTML = html[4];
+    project5.innerHTML = html[3];
+    project6.innerHTML = html[2];
+
 });
 
 
-    // another way:
-        // console.log(entry);
-        // let title = document.createElement('h1');
-        // title.textContent = entry.fields.title;
-        // blog.append(title);
+{/* <img class = "indiv-main-image" src="${entry.fields.mainImage.fields.file.url}">
+<video width="320" height="240" autoplay muted>
+    <source src="${entry.fields.mainVideo.fields.file.url}" type/mp4>
+</video>
+<div class = "indiv-body">${entry.fields.body}</div>
 
-        // let image = document.createElement('img');
-        // image.src = entry.fields.image.fields.file.url;
-        // image.classList.add('blog-img');
-        // blog.append(image);
+<img class = "indiv-image-1" src="${entry.fields.gallery[0].fields.file.url}">
+<img class = "indiv-image-2" src="${entry.fields.gallery[1].fields.file.url}">
 
-        // let body = document.createElement('p');
-        // body.textContent = entry.fields.body;
-        // blog.append(body
-        
+<div class = "indiv-extended-role">${entry.fields.notable}</p>
+<div class = "indiv-extended-role">${entry.fields.roleExtended}</p> */}
