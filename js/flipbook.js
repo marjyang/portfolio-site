2741596ec7d72d6d11a7d8a7642f11b0eea06344
marjyang <marjorie.y@hotmail.com>
2022-12-12 01:20:11 -0500

    mediaquery updates

--- js/flipbook.js
+++ js/flipbook.js
@@ -1,5 +1,9 @@
 const forwardBtn = document.getElementById('forward-button');
 const backBtn = document.getElementById('back-button');
+const artSection = document.getElementById('art');
+let verticalLine1 = document.getElementById("vertical1");
+let verticalLine2 = document.getElementById("vertical2");
+
 
 let count = 0;
 loadImage();
@@ -36,7 +40,6 @@ function loadImage(){
     let flipbook = document.getElementById("flipbook");
     let images = flipbook.querySelectorAll("div");
 
-
     //make sure none are showing
     for (let i = 0; i < images.length; i++){
         images[i].style.display = "none";
@@ -58,3 +61,21 @@ function loadImage(){
         isShowing = false;
     }
 }
+
+
+function mediaQuery1(x) {
+    if (x.matches) { // If media query matches
+        artSection.style.height = "70vh";
+        verticalLine1.style.height = "170vh";
+        verticalLine2.style.height = "170vh";
+
+    } else {
+        artSection.style.height = "100vh";
+        verticalLine1.style.height = "200vh";
+        verticalLine2.style.height = "200vh";
+    }
+  }
+  
+  var x = window.matchMedia("(max-width: 1000px)");
+  mediaQuery1(x);
+  x.addListener(mediaQuery1);
